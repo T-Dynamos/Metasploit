ab98bfa41a08f5c11d97aa44ffb949a1a122040f
T-Dynamos <68729523+T-Dynamos@users.noreply.github.com>
2021-07-10 09:46:06 +0530

    Add files via upload

---
+++ ins.sh
@@ -0,0 +1,40 @@
+cd $HOME/metasploit-framework
+sed '/rbnacl/d' -i Gemfile.lock
+sed '/rbnacl/d' -i metasploit-framework.gemspec
+gem install bundler
+sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+
+gem install nokogiri -v 1.8.0 -- --use-system-libraries
+
+gem install actionpack
+bundle update activesupport
+bundle update --bundler
+bundle install -j$(nproc --all)
+$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
+rm ./modules/auxiliary/gather/http_pdf_authors.rb
+if [ -e $PREFIX/bin/msfconsole ];then
+	rm $PREFIX/bin/msfconsole
+fi
+if [ -e $PREFIX/bin/msfvenom ];then
+	rm $PREFIX/bin/msfvenom
+fi
+ln -s $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
+ln -s $HOME/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
+ln -s $HOME/metasploit-framework/msfupdate /data/data/com.termux/files/usr/bin/
+ln -s $HOME/metasploit-framework/msfdb /data/data/com.termux/files/usr/bin/
+ln -s $HOME/metasploit-framework/msfd /data/data/com.termux/files/usr/bin/
+ln -s $HOME/metasploit-framework/msfrpc /data/data/com.termux/files/usr/bin/
+termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/2.4.0/gems/pg-0.20.0/lib/pg_ext.so
+cd $HOME/metasploit-framework/config
+curl -sLO https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/database.yml
+
+mkdir -p $PREFIX/var/lib/postgresql
+initdb $PREFIX/var/lib/postgresql
+
+pg_ctl -D $PREFIX/var/lib/postgresql start
+createuser msf
+createdb msf_database
+
+cd $HOME
+curl -sLO https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/postgresql_ctl.sh
+chmod +x postgresql_ctl.sh
---
+++ ruby.sh
@@ -0,0 +1,24 @@
+#!/data/data/com.termux/files/usr/bin/sh
+echo 'deb https://github.com/gushmazuko/metasploit_in_termux/raw/master gushmazuko main'  | tee $PREFIX/etc/apt/sources.list.d/gushmazuko.list
+
+pkg install -y gnupg
+curl -fsSL https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/gushmazuko-gpg.pubkey | gpg --dearmor | tee $PREFIX/etc/apt/trusted.gpg.d/gushmazuko-repo.gpg
+
+# Set low priority for all gushmazuko repository (for security purposes)
+# Set highest priority for ruby package from gushmazuko repository
+echo '## Set low priority for all gushmazuko repository (for security purposes)
+Package: *
+Pin: release gushmazuko
+Pin-Priority: 100
+
+## Set highest priority for ruby package from gushmazuko repository
+Package: ruby
+Pin: release gushmazuko
+Pin-Priority: 1001' | tee $PREFIX/etc/apt/preferences.d/preferences
+
+# Purge installed ruby
+apt purge ruby -y
+rm -fr $PREFIX/lib/ruby/gems
+source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
+
+pkg upgrade -y -o Dpkg::Options::="--force-confnew"
