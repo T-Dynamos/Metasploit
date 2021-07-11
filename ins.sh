fa0701a9be3d9203eefcd4ea3570da939f9ca7a4
T-Dynamos <68729523+T-Dynamos@users.noreply.github.com>
2021-07-11 12:15:04 +0530

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -5,7 +5,7 @@ gem install bundler
 sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
 
 gem install nokogiri -- --use-system-libraries
-
+gem install nokogiri -v 1.8.0 -- --use-system-libraries
 gem install actionpack
 bundle update activesupport
 bundle update --bundler
